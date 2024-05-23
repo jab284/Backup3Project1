125cd4bf1701195a65b634aed4466d45656a1dfe
jab284 <jab284testing@yahoo.com>
2024-05-23 16:00:34 -0400

    app setup

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    // Application setup
+        static UserService userService;
+        static TodoService todoService;     
+        static UserController userController; 
+        static ToDoController todoController;
+
+        //Main Method----------
+    static void Main(string[] args)
+    {
+        // Main method code here
+    }
+}
